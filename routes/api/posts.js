9f293fc6d8ab468260127629c0c0b9a810d1afe4
Developer994 <rez_a@outlook.com>
2022-02-20 15:14:33 -0500

    Added validator to check for user credentials such as name, email and password.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,8 +2,11 @@ const express = require('express')
 const router = express.Router();
 
 // @route  GET api/posts
-// @desc   Test Route
+// @desc   Test user
 // @access Public
-router.get('/', (req, res) => res.send('Posts Route'));
+
+router.get('/', (req, res) => {
+res.send('Posts Route')
+});
 
 module.exports = router;
