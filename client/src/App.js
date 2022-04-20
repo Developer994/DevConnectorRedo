522fa4cfcdebe32cdb3db09a8ae36f0950d9f201
Rezwan Atcha <rez_a@outlook.com>
2022-04-20 12:41:58 -0400

    Started working on Redux. Created Redux store and added alert reducer and alert component.

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,27 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <section className='container'>
+  <Provider store={store}>
+    <Alert />
+    <Router>
+      <Fragment>
+        <Navbar />
+
         <Routes>
-          <Route exact path='/' element={<Landing />} />
-          <Route exact path='/register' element={<Register />} />
-          <Route exact path='/login' element={<Login />} />
+          <Route path='/' element={<Landing />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
         </Routes>
-      </section>
-    </Fragment>
-  </Router>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
