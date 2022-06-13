1c6d20d90123cd53d9da09caa558085b38aa5f4c
Rezwan Atcha <rez_a@outlook.com>
2022-06-13 11:57:43 -0400

    Completed Profile Actions & Reducer

---
+++ client/src/components/profiles/Profiles.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const profiles = (props) => {
+  return <div>profiles</div>;
+};
+
+profiles.propTypes = {};
+
+export default profiles;
